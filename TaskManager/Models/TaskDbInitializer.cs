e3dbebb858c51d39a78c837da40246ff978f525e
Anton Klepikov <aklepikov@oneincsystems.com>
2021-04-22 01:38:57 +0500

    Mark project done

--- TaskManager/Models/TaskDbInitializer.cs
+++ TaskManager/Models/TaskDbInitializer.cs
@@ -6,7 +6,7 @@ namespace TaskManager.Models
     {
         protected override void Seed(TaskContext db)
         {
-            db.Tasks.Add(new Task("Get this project done", false));
+            db.Tasks.Add(new Task("Get this project done", true));
             db.Tasks.Add(new Task("Watch MLP", false));
             db.Tasks.Add(new Task("Work it Make it Do it Makes us Harder Better Faster Stronger More than Hour Hour Never Ever After Work is Over Work it Make it Do it Makes us Harder Better Faster Stronger Work it harder, make it better Do it faster, makes us stronger More than ever, hour after hour Work is never over Work it harder, make it better Do it faster, makes us stronger More than ever, hour after hour Work is never over Work it harder, make it better Do it faster, makes us stronger More than ever, hour after hour Work is never over Work it harder, make it better Do it faster, makes us stronger More than ever, hour after hour Work is never over Work it harder, make it better Do it faster, makes us stronger More than ever, hour after hour Work is never over Work it harder, make it better Do it faster, makes us… ", true));
 
